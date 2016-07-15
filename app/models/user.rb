21a7b8895033768c9d3dd53f4700eb9584c1b769
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-16 03:00:58 +0900

    例のごとくエラーだったので:omniauthableを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   has_many :posts, dependent: :destroy
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
