e0192f521819d411813d3e56816047ce90ca0c0b
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-17 01:56:27 +0900

    userのマイページ作成, マイページからのみユーザ情報編集

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
+    passwords: 'users/passwords',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
+  resources :users, only: [:show]
 
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
 
